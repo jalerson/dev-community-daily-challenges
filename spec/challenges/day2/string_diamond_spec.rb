7daf45b99121860161e3b7801531513d4b4a7b6d
Jalerson Lima <jalerson.lima@creditshelf.com>
2019-09-08 16:23:42 +0200

    Update tests

--- spec/challenges/day2/string_diamond_spec.rb
+++ spec/challenges/day2/string_diamond_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe Day2::StringDiamond do
-  subject { Day2::StringDiamond.new }
-
   context 'odd number' do
     context '5 as input' do
       it 'returns a 5-size diamond' do
-        expect(subject.call(5)).to match_array(
+        subject = Day2::StringDiamond.new(5)
+
+        expect(subject.call).to match_array(
           [
             [' ', ' ', '*', ' ', ' '],
             [' ', '*', '*', '*', ' '],
@@ -18,7 +18,9 @@ RSpec.describe Day2::StringDiamond do
 
     context '3 as input' do
       it 'returns a 3-size diamond' do
-        expect(subject.call(3)).to match_array(
+        subject = Day2::StringDiamond.new(3)
+
+        expect(subject.call).to match_array(
           [
             [' ', '*', ' '],
             ['*', '*', '*'],
@@ -31,13 +33,17 @@ RSpec.describe Day2::StringDiamond do
 
   context 'even number' do
     it 'returns nil' do
-      expect(subject.call(4)).to be_nil
+      subject = Day2::StringDiamond.new(4)
+
+      expect(subject.call).to be_nil
     end
   end
 
   context 'negative number' do
     it 'returns nil' do
-      expect(subject.call(-3)).to be_nil
+      subject = Day2::StringDiamond.new(-3)
+
+      expect(subject.call).to be_nil
     end
   end
 end
